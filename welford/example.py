Fixes bug with remove. Now it appears the remove algorithm is numerically stable.
In welford.remove, I was using self.__old_m as the previous m, but that caused a bug.
So instead, I manually store the previous m as old_m = np.copy(self.__m). This fixes the bug
and now it appears the algorithm is numerically stable. Running test_welford.test_remove with a sample
size of 1,000,000 resulted in no assertion errors, thus I believe the modified algorithm is numerically
stable. Again, I have done no formal analysis on this, but imperically it seems to work.

welford/example.py
@@ -11,29 +11,29 @@ w.add(np.array([1, 110]))
 w.add(np.array([2, 120]))
 
 # output
-print(w.mean)  # mean --> [  1. 110.]
-print(w.var_s)  # sample variance --> [1, 100]
-print(w.var_p)  # population variance --> [ 0.6666 66.66]
+print(w.mean)  # mean --> [1. 110.]
+print(w.var_s)  # sample variance --> [1. 100.]
+print(w.var_p)  # population variance --> [0.6666. 66.66.]
 
 # You can add other samples after calculating variances.
 w.add(np.array([3, 130]))
 w.add(np.array([4, 140]))
 
 # output with added samples
-print(w.mean)  # mean --> [  2. 120.]
-print(w.var_s)  # sample variance --> [  2.5 250. ]
-print(w.var_p)  # population variance --> [  2. 200.]
+print(w.mean)  # mean --> [2. 120.]
+print(w.var_s)  # sample variance --> [2.5. 250.]
+print(w.var_p)  # population variance --> [2. 200.]
 
-# You can remove other sample after calculating variances.
+# You can remove samples after calculating variances.
 w.remove(np.array([3, 130]))
 w.remove(np.array([4, 140]))
-print(w.mean)  # mean --> [  1. 110.]
-print(w.var_s)  # sample variance --> [1, 100]
-print(w.var_p)  # population variance --> [ 0.6666 66.66]
+print(w.mean)  # mean --> [1. 110.]
+print(w.var_s)  # sample variance --> [1. 100.]
+print(w.var_p)  # population variance --> [0.6666. 66.66.]
 
 # You can also get the standard deviation
-print(w.std_s)  # sample standard deviation --> [1, 10]
-print(w.std_p)  # population standard deviation --> [0.81649658 8.16496581]
+print(w.std_s)  # sample standard deviation --> [1. 10.]
+print(w.std_p)  # population standard deviation --> [0.81649658. 8.16496581.]
 # %%
 import numpy as np
 from welford import Welford
@@ -43,18 +43,18 @@ ini = np.array([[0, 100], [1, 110], [2, 120]])
 w = Welford(ini)
 
 # output
-print(w.mean)  # mean --> [  1. 110.]
-print(w.var_s)  # sample variance --> [1, 100]
-print(w.var_p)  # population variance --> [ 0.66666667 66.66666667]
+print(w.mean)  # mean --> [1. 110.]
+print(w.var_s)  # sample variance --> [1. 100.]
+print(w.var_p)  # population variance --> [0.66666667. 66.66666667.]
 
 # add other samples through batch method
 other_samples = np.array([[3, 130], [4, 140]])
 w.add_all(other_samples)
 
 # output with added samples
-print(w.mean)  # mean --> [  2. 120.]
-print(w.var_s)  # sample variance --> [  2.5 250. ]
-print(w.var_p)  # population variance --> [  2. 200.]
+print(w.mean)  # mean --> [2. 120.]
+print(w.var_s)  # sample variance --> [2.5 250.]
+print(w.var_p)  # population variance --> [2. 200.]
 
 
 # %%
@@ -70,18 +70,17 @@ w_2 = Welford()
 w_1.add(np.array([0, 100]))
 w_1.add(np.array([1, 110]))
 w_1.add(np.array([2, 120]))
-print(w_1.var_s)  # sample variance -->[  1. 100.]
-print(w_1.var_p)  # population variance -->[ 0.66666667 66.66666667]
+print(w_1.var_s)  # sample variance --> [1. 100.]
+print(w_1.var_p)  # population variance --> [0.66666667. 66.66666667.]
 
 # On w_2
 w_2.add(np.array([3, 130]))
 w_2.add(np.array([4, 140]))
-print(w_2.var_s)  # sample variance -->[ 0.5 50. ]
-print(w_2.var_p)  # sample variance -->[ 0.25 25.  ]
+print(w_2.var_s)  # sample variance --> [0.5 50.]
+print(w_2.var_p)  # sample variance --> [0.25 25.]
 
 # You can Merge objects to get variance of WHOLE samples
 w_1.merge(w_2)
-print(w.var_s)  # sample variance --> [  2.5 250. ]
-print(w_1.var_p)  # sample variance -->[  2. 200.]
-
+print(w.var_s)  # sample variance --> [2.5. 250.]
+print(w_1.var_p)  # sample variance --> [2. 200.]
 # %%

welford/test_welford.py
@@ -51,6 +51,7 @@ def test_add():
     assert np.allclose(w.var_s, np.array([[0.5, 50, 5000], [0, 0, 0]]))
     assert np.allclose(w.var_p, np.array([[0.25, 25, 2500], [0, 0, 0]]))
 
+
 def test_add_all():
     w = Welford()
     a = np.array([[0, 100], [1, 110], [2, 120], [3, 130], [4, 140]])
@@ -195,8 +196,9 @@ def test_merge_both_empty():
 def test_add_many():
     w = Welford()
     running_sample = []
+    N = 1000
 
-    for i in range(0, 1000):
+    for i in range(0, N):
         random_number = np.array([np.random.rand()])
         running_sample.append(random_number)
         w.add(random_number)
@@ -212,25 +214,16 @@ def test_add_many():
 
 
 def test_remove():
-    _test_remove_k_elements(1)
-    # _test_remove_k_elements(3)
-
-
-def _test_remove_k_elements(k: int):
-    random_sample = np.random.normal(0, 1, 1000)
+    N = 100000
+    random_sample = np.random.rand(N)
     w = Welford(random_sample)
 
-    for i in range(len(random_sample) - k):
-        # print(i)
-        # print(w.count)
-        indices_to_remove = list(range(i, i + k))
-        sample = np.delete(random_sample, indices_to_remove)
-        w.remove_all(np.take(random_sample, indices_to_remove))
+    for i in range(0, N - 2):
+        w.remove(random_sample[0])
+        random_sample = random_sample[1:]
 
-        std = np.std(sample, ddof=1, dtype=np.longdouble)
-        mean = np.mean(sample, dtype=np.longdouble)
+        std = np.std(random_sample, ddof=1, dtype=np.longdouble)
+        mean = np.mean(random_sample, dtype=np.longdouble)
 
         assert np.isclose(std, w.std_s)
         assert np.isclose(mean, w.mean)
-
-        w.rollback()

welford/welford.py
@@ -6,16 +6,27 @@ This library is a Python (Numpy) implementation of a modified Welford's algorith
 which is online and parallel algorithm for calculating variances. Typically, Welford's algorithm
 only allows for adding data points. This modification allows for removing data points.
 
-Welford's method is more numerically stable than the standard method as
-described in the following blog, However, there has been no analysis on whether
-removing data points through the modification provided is numerically stable.
+Welford's algorithm is described in the following:
+
+* https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Online_algorithm
+* https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance#Parallel_algorithm
+
+The modification for removing data points is described here:
+* https://stackoverflow.com/questions/30876298/removing-a-prior-sample-while-using-welfords-method-for-computing-single-pass-v
+
+Welford's original method is more numerically stable than the standard method as
+described in the following blog.
     * Accurately computing running variance: www.johndcook.com/blog/standard_deviation
+
+However, There has been no formal analysis on whether
+the modified version of the algorithm provided here is numerically stable, but based
+on the testing done in test_welford.test_remove, I have reason to believe it is.
 
 This library is inspired by the jvf's implementation, which is implemented
 without using numpy library. In particular, this implementation is a fork
 of the implementation by a-mitani,
-    * implementation done by jvf: github.com/jvf/welford
-    * implementation done by a-mitani: github.com/a-mitani/welford
+    * Implementation done by jvf: github.com/jvf/welford
+    * Implementation done by a-mitani: github.com/a-mitani/welford
 """
 from __future__ import annotations
 
@@ -173,8 +184,9 @@ class Welford:
             self.__backup_attrs()
 
         # The reverse of Welford's algorithm.
+        old_m = np.copy(self.__m)
         self.__m -= (element - self.__m) / (self.count - 1)
-        self.__s -= (element - self.__m) * (element - self.__m_old)
+        self.__s -= (element - self.__m) * (element - old_m)
         self.__count -= 1
 
     def remove_all(self, elements: npt.NDArray[npt.NDArray], backup_flg: bool = True) -> None:
@@ -244,7 +256,7 @@ class Welford:
         filled with NaN values of the appropriate shape.
 
         Args:
-            ddof (int): The delta degrees of freedom.
+            ddof: The delta degrees of freedom.
 
         """
         if self.__count <= 0: